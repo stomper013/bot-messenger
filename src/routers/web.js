b3183b021f04c21659d2373eff1d357a98dedf99
HOANG-IOT <stomper013@gmail.com>
2020-12-22 15:47:15 +0700

    update

--- src/routers/web.js
+++ src/routers/web.js
@@ -1,12 +1,15 @@
-import express from 'express';
-import homePageController from '../controllers/homePageController';
- 
+import express from "express";
+import homepageController from "../controllers/homepageController";
+import chatBotController from "../controllers/chatBotController";
+
 let router = express.Router();
 
-let initWebRouter = (app) => {
-    router.get("/", homePageController.getHomepage);
-    
+let initWebRoutes = (app)=> {
+    router.get("/", homepageController.getHomepage);
+    router.get("/webhook", chatBotController.getWebhook);
+    router.post("/webhook", chatBotController.postWebhook);
+
     return app.use("/", router);
 };
 
-module.exports = initWebRouter; 
+module.exports = initWebRoutes;
