6f53b46636f1098d265dee97850d5306187d949d
HOANG-IOT <stomper013@gmail.com>
2020-12-23 23:03:05 +0700

    123

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -101,7 +101,7 @@ function callSendAPI(sender_psid, response) {
     }, (err, res, body) => {
         if (!err) {
             console.log('message sent!');
-            console.log(`My message: ${response}`);
+            console.log(`My message: ${response.text}`);
         } else {
             console.error("Unable to send message:" + err);
         }
