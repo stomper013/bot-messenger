3aa7999ebe2fa8fd93bf93667fc588f2ef75e50b
HOANG-IOT <stomper013@gmail.com>
2020-12-25 15:41:34 +0700

    test

--- src/routers/web.js
+++ src/routers/web.js
@@ -9,7 +9,7 @@ router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
 let initWebRoutes = (app)=> {
-    router.get("/", homepageController.getHomePage);
+    router.get("/", homepageController.getHomepage);
     router.get("/webhook", chatBotController.getWebhook);
     router.post("/webhook", chatBotController.postWebhook);
 
