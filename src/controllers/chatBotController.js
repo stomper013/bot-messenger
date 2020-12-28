12ae54d79c875f9cf02048e05662c7f1350d6037
stomper013 <stomper013@gmail.com>
2020-12-28 11:08:16 +0700

    123

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -14,11 +14,12 @@ export let postWebhook = (req, res) =>{
             // Gets the body of the webhook event
             let webhook_event = entry.messaging[0];
             console.log('webhook event: ' + webhook_event.message.text);
-            // let newMessage = new Messenger(body.entry.messaging[0]);
-            // newMessage.save(function (err, message) {
-            //     if(err) res.send(err);
-            //     res.json(message);
-            // });
+
+            let newMessage = new Messenger(webhook_event.message.text);
+            newMessage.save(function (err, message) {
+                if(err) res.send(err);
+                res.json(message);
+            });
 
             // Get the sender PSID
             let sender_psid = webhook_event.sender.id;
