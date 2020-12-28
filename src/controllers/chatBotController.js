8c4f208820d01ef6dc49577f1326f34130c2aa8b
stomper013 <stomper013@gmail.com>
2020-12-28 11:40:17 +0700

    123

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -120,10 +120,8 @@ function handlePostback(sender_psid, received_postback) {
     let payload = received_postback.payload;
 
     // Set the response based on the postback payload
-    if (payload === 'yes') {
-        response = { "text": "Thanks!" }
-    } else if (payload === 'no') {
-        response = { "text": "Oops, try sending another image." }
+    if(payload === 'GET_STARTED'){
+        response = { "text": "Get started"}
     }
     // Send the message to acknowledge the postback
     callSendAPI(sender_psid, response);
