733a44377ddd28380544b1f88df6c36e04f657ca
stomper013 <stomper013@gmail.com>
2020-12-28 16:33:19 +0700

    123

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,2 @@
-const mongoose = require("mongoose");
-const MONGO_URI =
-  "mongodb+srv://admin:admin@cluster0.uadkt.mongodb.net/FB?retryWrites=true&w=majority";
-const connectDB = async () => {
-  const conn = await mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  });
-  console.log(`MongoDB Connected: ${conn.connection.host}`);
-};
+
 module.exports = connectDB;
--- src/model/messenger.js
+++ src/model/messenger.js
@@ -1,6 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
+const MONGO_URI =
+  "mongodb+srv://admin:admin@cluster0.uadkt.mongodb.net/FB?retryWrites=true&w=majority";
+const connectDB = async () => {
+  const conn = await mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+  console.log(`MongoDB Connected: ${conn.connection.host}`);
+};
+
 const messengerSchema = new Schema({
     value: String
 });
