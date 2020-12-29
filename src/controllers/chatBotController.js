98c10c273818620e149d15ae712157c387113603
stomper013 <stomper013@gmail.com>
2020-12-29 09:33:37 +0700

    123

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -22,9 +22,21 @@ export let postWebhook = (req, res) =>{
             // Gets the body of the webhook event
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
+
+        // Add database in mongoose
+            messenger.findOne({ sender_id: sender_id}, function(err) {
+                if (err) {
+                    console.log(err);
+                }else {
+                    var newMessage = new messenger({
+                        message: mess, 
+                        sender_id: sender_id, 
+                        recipient_id: recipient_id, 
+                        timestamp: timestamp});
+                    newMessage.save();
+                }
+            })
             
-            var newMessage = new messenger({message: mess, sender_id: sender_id, recipient_id: recipient_id, timestamp: timestamp});
-            newMessage.save();
 
             // Get the sender PSID
             let sender_psid = webhook_event.sender.id;
