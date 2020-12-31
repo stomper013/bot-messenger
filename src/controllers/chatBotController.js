424150ec028395811f9639db7755d0c037ea98cd
stomper013 <stomper013@gmail.com>
2020-12-31 09:36:56 +0700

    123

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -30,7 +30,7 @@ export let postWebhook = (req, res) =>{
                     console.log("errrrrrr",err);
                 }else {
                     if(res._id == id_mongo){
-                        console.log('ok123')
+                        console.log('ok123',res._id)
                     }else{
                         console.log('no')
                     }
